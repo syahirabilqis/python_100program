303077b962aecb784448ea7d019af9121f948c32
= <=>
2024-10-09 10:42:27 +0700

    create aplikasi segitiga

--- segitiga.py
+++ segitiga.py
@@ -1,3 +1,16 @@
 # Dibuat oleh : Syahira Bilqis Humaira
 # Tanggal pengerjaan :  9 Oktober 2024
-# Program Persegi
+# Program Segitiga
+
+print("*"*40)
+print("Program Segitiga")
+
+Tinggi = int(input("Masukan Tinggi: "))
+Alas = int(input("Masukan Alas: "))
+Sisi = int(input("Masukan Sisi: "))
+
+Luas = 1/2 * Alas * Tinggi
+Keliling = Sisi * Sisi * Sisi
+
+print("Luas: ",Luas, "m2")
+print("Keliling: ",Keliling, "m")
